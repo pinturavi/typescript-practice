cdb7a1ededb468f6988927a4efcef7cdf169ccf2
pintu ravi <pintu541796@gmail.com>
2018-02-07 07:00:40 +0530

    Initial commit

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello " + person;
+}
+var user = "pintu ravi";
+document.body.innerHTML = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,6 @@
+function greeter(person){
+    return "Hello "+person;
+}
+
+let user = "pintu ravi";
+document.body.innerHTML = greeter(user);
